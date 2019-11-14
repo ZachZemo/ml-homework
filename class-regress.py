e3f92d8882f5ccc964850ef73c5978847e1135e4
Zach Zemokhol <45083180+SoyStillGood@users.noreply.github.com>
2019-11-14 17:52:36 -0500

    ML Commit

--- class-regress.py
+++ class-regress.py
@@ -17,16 +17,41 @@ print(wine.target)
 y = wine.target
 X = wine.data
 
-# Splitting features and target datasets into: train and test
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.35)
 
-# Training a Linear Regression model with fit()
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+
+
 from sklearn.linear_model import LinearRegression
 lm = LinearRegression()
 lm.fit(X_train, y_train)
 
-# Output of the training is a model: a + b*X0 + c*X1 + d*X2 ...
+
 print(f"Intercept: {lm.intercept_}\n")
 print(f"Coeficients: {lm.coef_}\n")
 print(f"Named Coeficients: {pd.DataFrame(lm.coef_, columns_names)}")
+
+
+from sklearn.linear_model import LogisticRegression
+
+lr = LogisticRegression()
+lr.fit(X_train, y_train)
+
+
+print(f"Intercept per class: {lr.intercept_}\n")
+print(f"Coeficients per class: {lr.coef_}\n")
+
+print(f"Available classes: {lr.classes_}\n")
+print(f"Named Coeficients for class 1: {pd.DataFrame(lr.coef_[0], columns_names)}\n")
+print(f"Named Coeficients for class 2: {pd.DataFrame(lr.coef_[1], columns_names)}\n")
+print(f"Named Coeficients for class 3: {pd.DataFrame(lr.coef_[2], columns_names)}\n")
+
+print(f"Number of iterations generating model: {lr.n_iter_}")
+
+from sklearn import neighbors, datasets
+
+
+knn = neighbors.KNeighborsClassifier(n_neighbors=5)
+knn.fit(X, y)
+
+print(knn.score(X_test, y=y_test))
